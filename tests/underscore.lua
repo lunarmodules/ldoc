8e791b8be6e52a203bc48ef8c3436092a24ae3a4
steve donovan <steve.j.donovan@gmail.com>
2011-07-05 20:19:48 +0200

    new md test

---
+++ tests/underscore.lua
@@ -0,0 +1,25 @@
+--[[-----------
+ testing underscores
+
+  - a list
+     * sublist
+     * more
+  * of items
+  more than one line
+  - indeed
+  can keep going
+
+Look at <<_ONE_>> and <<table.concat>>
+
+ @module underscore
+]]
+
+----------
+-- A function (<<_TWO_>>).
+-- @see _TWO_
+function _ONE_() end
+
+-----------
+-- another function
+-- @see string.format
+function _TWO_() end
