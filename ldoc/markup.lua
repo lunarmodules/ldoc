99cfca7643017a19adba5a69c0a7f5139a2eda9c
steve donovan <steve.j.donovan@gmail.com>
2011-07-05 18:53:28 +0200

    attempt to tame markdown.lua's need for blank lines around lists; it is not doing nested lists correctly (throwing in <p> inside <li>)

--- ldoc/markup.lua
+++ ldoc/markup.lua
@@ -19,6 +19,20 @@ function markup.create (ldoc, format)
          local res = ('[%s](%s)'):format(label,ldoc.href(ref))
          return res
       end)
+      if txt:find '\n' then -- multiline text
+         local res = {}
+         local last_line, last_indent = '', 0
+         for line in stringx.lines(txt) do
+            local indent = #line:match '^%s*'
+            local litem = line:match '^%s%s*[%-%*]'
+            if (litem and indent > last_indent) or (not litem and indent < last_indent) then
+               table.insert(res,'')
+            end
+            table.insert(res,line)
+            last_line, last_indent = line, indent
+         end
+         txt = table.concat(res,'\n')
+      end
       txt = markup(txt)
       -- We will add our own paragraph tags, if needed.
       return (txt:gsub('^%s*<p>(.+)</p>%s*$','%1'))
