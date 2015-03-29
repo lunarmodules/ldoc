134313e9a0297cebf79c44610d53a5ff061c062e
xelivous <mail@xelivo.us>
2015-03-28 23:31:42 -0500

    add missing parenthesis in ldoc_ltp.lua

--- ldoc/html/ldoc_ltp.lua
+++ ldoc/html/ldoc_ltp.lua
@@ -65,7 +65,7 @@ return [==[
 # for kind, mods, type in ldoc.kinds() do
 #  if ldoc.allowed_in_contents(type,module) then
 <h2>$(kind)</h2>
-<ul class="$(kind=='Topics' and '' or 'nowrap'">
+<ul class="$(kind=='Topics' and '' or 'nowrap')">
 #  for mod in mods() do local name = display_name(mod)
 #   if mod.name == this_mod then
   <li><strong>$(name)</strong></li>
