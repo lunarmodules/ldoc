39f68feb4151b005e0b4a60bbb574c31c95a40b4
BiterUnion <biterunion.github@collectmail.org>
2022-09-10 12:49:55 +0200

    Add test using type
    Adds a test using @type in @module and @submodule.

---
+++ tests/type/SimpleModule.lua
@@ -0,0 +1,23 @@
+-----------
+-- Simple module.
+-- This contains two types `Type1InModule` and `Type2InModule`,
+-- that are both listed as classes in the generated docs.
+-- @module CombinedModule
+
+--- Type1InModule description.
+-- @type Type1InModule
+Type1InModule = {}
+
+--- Type1InModule:A description.
+function Type1InModule:A()
+
+end
+
+--- Type2InModule description.
+-- @type Type2InModule
+Type2InModule = {}
+
+--- Type2InModule:A description.
+function Type2InModule:B()
+
+end
